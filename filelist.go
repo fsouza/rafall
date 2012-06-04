7c661933980aec9ab1677f7891e9f21b0929086e
Francisco Souza <f@souza.cc>
2012-06-04 15:25:06 -0300

    all: added docs for functions, methods and types

--- filelist.go
+++ filelist.go
@@ -4,20 +4,33 @@
 
 package main
 
+// FileList represents a list of files in memory.
+//
+// Each file contains a Metadata and a slice of bytes that represents the
+// content, so FileList encapsulates these two informations as a file, using
+// two internal slices.
+//
+// FileList implements sort.Interface, so it is sortable using sort.Sort.
 type FileList struct {
 	metas    []Metadata
 	contents [][]byte
 }
 
+// Append adds a file to the internal list.
+//
+// The file is identified by its metadata and content.
 func (fl *FileList) Append(m Metadata, content []byte) {
 	fl.metas = append(fl.metas, m)
 	fl.contents = append(fl.contents, content)
 }
 
+// Len is the number of elements in the list.
 func (fl *FileList) Len() int {
 	return len(fl.metas)
 }
 
+// Less returns whether the element with index x should sort before the
+// element with index y.
 func (fl *FileList) Less(x, y int) bool {
 	if x < 0 || y < 0 {
 		return false
@@ -31,6 +44,7 @@ func (fl *FileList) Less(x, y int) bool {
 	return meta1.Date.Before(meta2.Date.Time)
 }
 
+// Swap swaps the elements with indexes x and y.
 func (fl *FileList) Swap(x, y int) {
 	length := fl.Len()
 	if x >= 0 && x <= length && y >= 0 && y <= length && x != y {
--- rafall.go
+++ rafall.go
@@ -21,15 +21,19 @@ var (
 	jsonEnd   = []byte("}-->")
 )
 
-// Time is used to parse dates in RFC822 format from JSON
+// Time is used to parse dates in RFC822 format from JSON.
 type Time struct {
 	time.Time
 }
 
+// MarshalJSON implements the json.Marshaler interface, formatting time
+// using RFC822 format.
 func (t *Time) MarshalJSON() ([]byte, error) {
 	return []byte(t.Format(`"` + time.RFC822Z + `"`)), nil
 }
 
+// UnmarshalJSON implements the json.Unmarshaller interface, formatting time
+// using RFC822 format.
 func (t *Time) UnmarshalJSON(data []byte) (err error) {
 	var tim time.Time
 	if tim, err = time.Parse(`"`+time.RFC822Z+`"`, string(data)); err == nil {
@@ -38,12 +42,28 @@ func (t *Time) UnmarshalJSON(data []byte) (err error) {
 	return
 }
 
+// Metadata represents the metadata of a file.
+//
+// Each file should declare its metadata in JSON format in the beggining of
+// the file, using HTML comments. For example:
+//
+//     <!--{
+//     "Title": "My post",
+//     "Date": "04 Jun 12 13:56 -0300",
+//     "Tags": ["post", "blog"]
+//     }-->
+//
+// Notice that this code should be the first thing in the code.
 type Metadata struct {
 	Title string
 	Date  Time
 	Tags  []string
 }
 
+// extractMetadata extracts Metadata from a slice of bytes.
+//
+// The slice is supposed to be the content of the file. It returns the
+// content minus the metadata, the metadata and an error, if any happens.
 func extractMetadata(content []byte) (tail []byte, meta Metadata, err error) {
 	tail = content
 	if !bytes.HasPrefix(content, jsonStart) {
@@ -55,16 +75,28 @@ func extractMetadata(content []byte) (tail []byte, meta Metadata, err error) {
 	return tail[end+len(jsonEnd):], meta, err
 }
 
+// readConfig reads config from a slice of bytes. The slice of bytes should
+// be in JSON format. It is supposed to be the content from the config file.
 func readConfig(content []byte) (config map[string]string, err error) {
 	err = json.Unmarshal(content, &config)
 	return
 }
 
+// Generator represents the site generator.
+//
+// This type encapsulates the site configuration, and provide the methods
+// that should be used to (re)generate the site.
 type Generator struct {
 	config    map[string]string
 	metaFiles map[string]string
 }
 
+// NewGenerator returns a new generator instance, and an error, if any.
+//
+// It receives the configuration file name. It should be a path, that can be
+// absolute or relative. If the file does not exist, or is not in JSON
+// format, or other kind of unexpected error happen, this function returns
+// error.
 func NewGenerator(configFile string) (g Generator, err error) {
 	f, err := os.Open(configFile)
 	if err != nil {
@@ -84,6 +116,9 @@ func NewGenerator(configFile string) (g Generator, err error) {
 	return
 }
 
+// isMetaFile indicates whether the given filename represents a metafile. A
+// metafile is an special HTML file that can not be used as post. For
+// example, layout.html is the metafile that defines the layout of the site.
 func (g *Generator) isMetaFile(filename string) bool {
 	for _, v := range g.metaFiles {
 		if filename == v {
@@ -102,6 +137,12 @@ func (g *Generator) isValid(filename string) bool {
 	return !g.isMetaFile(filename)
 }
 
+// collectFiles collect files in the src directory, and returns a FileList
+// and an error, if any happens. The FileList contains all files contents
+// and metadata, and does not include metafiles.
+//
+// After using this method to collect the list of files, the generator
+// should render all files and store the output.
 func (g *Generator) collectFiles() (fl FileList, err error) {
 	dir, err := os.Open("src")
 	if err != nil {
